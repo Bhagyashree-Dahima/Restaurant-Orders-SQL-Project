b3833e131698c5dc052950f99ea95bafbb78e0f8
Bhagyahree Dahima <dahimabhagyashree02@gmail.com>
2025-10-03 20:37:33 +0530

    Add files via upload

---
+++ sql qoery.sql
@@ -0,0 +1,32 @@
+use restaurant_db;
+select* from menu_items
+
+--View the menu_items table .find the no of items
+select count(*) from menu_items
+
+--What are the least and most expensive items on the menu?
+select *  from  menu_items
+order by price;
+select *  from  menu_items
+order by price desc;
+
+--How many Italian dishes are on the menu? 
+select count(*) from  menu_items
+where category="Italian"
+
+--What are the least and most expensive Italian dishes on the menu?
+select * from  menu_items
+where category="Italian"
+order by price
+select * from  menu_items
+where category="Italian"
+order by price desc
+
+--How many dishes are in each category? What is the average dish price within each category?
+select category, count(menu_item_id) from  menu_items
+group by category
+
+select category, avg(price) from  menu_items
+group by category
+
+
